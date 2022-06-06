29464051d304530b63496271a858c32a993395c8
J.Manuel Zaragoza <zaragozajn@gmail.com>
2022-06-06 12:42:05 -0700

    BrowserRouter is now replaced with HashRouter inorder to work with GitHub pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import { Index as PackView } from './pack'
 import NotFoundPage from "./404/NotFoundPage";
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path={"/p/:hash"} element={<PackView/>}/>
-            <Route path={"*"} element={<NotFoundPage/>}/>
-        </Routes>
-    </BrowserRouter>
+      <Routes>
+          <Route path={"/p/:hash"} element={<PackView/>}/>
+          <Route path={"*"} element={<NotFoundPage/>}/>
+      </Routes>
   );
 }
 
